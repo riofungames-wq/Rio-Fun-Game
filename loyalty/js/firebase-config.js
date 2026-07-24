3ed49824a4946841710d4cac320ee597f147bd8c
riofungames-wq <riofungames@gmail.com>
2026-07-24 07:17:48 +0530

    Add Firebase configuration
    Added Firebase configuration for RIO LOYALTY CLUB.

--- loyalty/js/firebase-config.js
+++ loyalty/js/firebase-config.js
@@ -1,1 +1,20 @@
+// ======================================
+// RIO LOYALTY CLUB
+// FIREBASE CONFIG
+// ======================================
 
+export const firebaseConfig = {
+
+  apiKey: "AIzaSyB5ZmPaMW17YnUOvd48QhHXwS8it6XDB70",
+
+  authDomain: "rio-maggi-point.firebaseapp.com",
+
+  projectId: "rio-maggi-point",
+
+  storageBucket: "rio-maggi-point.firebasestorage.app",
+
+  messagingSenderId: "472858143171",
+
+  appId: "1:472858143171:web:15cfb0b8de9cd25b957576"
+
+};
